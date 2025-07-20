4183608e9a487628359d9aa77c425a3ea407c1fc
BlueUpQuark <justin.carlton96@gmail.com>
2025-07-20 04:50:24 -0400

    Added logo into window

--- BPC/MainFrame.cpp
+++ BPC/MainFrame.cpp
@@ -1,5 +1,23 @@
 #include "MainFrame.h"
+#include <wx/wx.h>
+
 
 MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, "Blue Physics Calculator", wxPoint(200, 200), wxSize(800, 600)) {
+	//wxInitAllImageHandlers();
 
+	wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+
+	wxPanel* headerPanel = new wxPanel(this, wxID_ANY);
+	headerPanel->SetBackgroundColour(wxColor(70, 130, 180));
+
+	wxBitmap logoBitmap("images/logo.bmp", wxBITMAP_TYPE_BMP);
+	wxStaticBitmap* logo = new wxStaticBitmap(headerPanel, wxID_ANY, logoBitmap);
+
+	wxBoxSizer* headerSizer = new wxBoxSizer(wxHORIZONTAL);
+	headerSizer->Add(logo, 0, wxALIGN_CENTER | wxALL, 10);
+	headerPanel->SetSizer(headerSizer);
+
+	
+
+	
 }
